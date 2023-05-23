4de5d150484a7d49e81f9dcaab9d9ce349e1cded
0svaldojunior <app.o.r.jr@gmail.com>
2023-05-23 15:18:16 -0300

    Finished

---
+++ server/src/server.ts
@@ -0,0 +1,41 @@
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import 'dotenv/config'
+import fastify from 'fastify'
+import { resolve } from 'path'
+import { authRoutes } from './routes/auth'
+import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
+
+const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads/',
+})
+
+app.register(cors, {
+  origin: true, // todas URLś de front-end poderão acessar nosso back-end
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
+app.register(uploadRoutes)
+app.register(memoriesRoutes)
+
+app
+  .listen({
+    port: 3333,
+    host: '0.0.0.0',
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
+
+// HTTP Method: GET(list they), POST(make it), PUT(att it), PATCH(att specific on it), DELETE(dele it)
---
+++ web/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/aquarian-pc/Insync/app.o.r.jr@gmail.com/Google Drive/Development/Rocketseat/NLW-14-Spacetime/web/src/app/layout.tsx": [
+      "/home/aquarian-pc/Insync/app.o.r.jr@gmail.com/Google Drive/Development/Rocketseat/NLW-14-Spacetime/web/src/app/globals.css",
+      "/home/aquarian-pc/Insync/app.o.r.jr@gmail.com/Google Drive/Development/Rocketseat/NLW-14-Spacetime/web/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Roboto_Flex\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-roboto\"}],\"variableName\":\"roboto\"}",
+      "/home/aquarian-pc/Insync/app.o.r.jr@gmail.com/Google Drive/Development/Rocketseat/NLW-14-Spacetime/web/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Bai_Jamjuree\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"700\",\"variable\":\"--font-bai-jamjuree\"}],\"variableName\":\"baiJamjuree\"}"
+    ]
+  },
+  "cssModules": {
+    "/home/aquarian-pc/Insync/app.o.r.jr@gmail.com/Google Drive/Development/Rocketseat/NLW-14-Spacetime/web/src/app/page": [
+      "/home/aquarian-pc/Insync/app.o.r.jr@gmail.com/Google Drive/Development/Rocketseat/NLW-14-Spacetime/web/src/app/globals.css",
+      "/home/aquarian-pc/Insync/app.o.r.jr@gmail.com/Google Drive/Development/Rocketseat/NLW-14-Spacetime/web/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Roboto_Flex\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-roboto\"}],\"variableName\":\"roboto\"}",
+      "/home/aquarian-pc/Insync/app.o.r.jr@gmail.com/Google Drive/Development/Rocketseat/NLW-14-Spacetime/web/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Bai_Jamjuree\",\"arguments\":[{\"subsets\":[\"latin\"],\"weight\":\"700\",\"variable\":\"--font-bai-jamjuree\"}],\"variableName\":\"baiJamjuree\"}"
+    ]
+  }
+}
